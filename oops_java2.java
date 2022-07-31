7c8c80ca85a5db2994f36911ac9959d0d711fd03
Maddy2218 <57710530+madhur2218@users.noreply.github.com>
2022-07-31 17:21:30 +0530

    Add files via upload

---
+++ oops_java2.java
@@ -0,0 +1,20 @@
+class Student {
+    String name;
+    int age;
+   
+    public void getInfo() {
+        System.out.println("The name of this Student is " + this.name);
+        System.out.println("The age of this Student is " + this.age);
+    }
+ }
+  
+ public class oops_java2 {
+    public static void main(String args[]) {
+        Student s1 = new Student();
+        s1.name = "Aman";
+        s1.age = 24;
+        s1.getInfo();
+  
+        Student s2 = new Student();
+    }
+}
